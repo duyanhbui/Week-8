53f9dd55880737a20170fff8921baf2aadf14c93
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-09-13 20:03:19 +0930

    second commit

--- Player.cpp
+++ Player.cpp
@@ -6,14 +6,17 @@ char Player :: getMove(){
 }
 
 bool Player :: win(Player *opponent){
-  std :: cout << opponent->getMove() << "::" << this->getMove() << std :: endl;
-  if( (opponent->getMove() && this->getMove()) || (opponent->getMove() == 'r' && this->getMove() == 's') 
-      || (opponent->getMove() == 'p' && this->getMove() == 'r') || (opponent->getMove() == 's' && this->getMove() == 'p')){
-    std :: cout << "False"  << std :: endl;
-    return false;
-  }
-  else{
-    std :: cout << "True"  << std :: endl;
+  //std :: cout << opponent->getMove() << "::" << this->getMove() << std :: endl;
+  if ((opponent->getMove() == 'r' && this->getMove() == 'p') 
+      || (opponent->getMove() == 's' && this->getMove() == 'r') || (opponent->getMove() == 'p' && this->getMove() == 's')){
+    //std :: cout << "True"  << std :: endl;
     return true;
   }
+  else if( (opponent->getMove() && this->getMove()) || (opponent->getMove() == 'r' && this->getMove() == 's') 
+      || (opponent->getMove() == 'p' && this->getMove() == 'r') || (opponent->getMove() == 's' && this->getMove() == 'p')){
+    //std :: cout << "False"  << std :: endl;
+    return false;
+  }
+
+  return false;
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -11,20 +11,27 @@ int main(){
   c1->win(p1);
   p1->win(c1);
 
-  /*
+  std :: cout << c1->getMoves() << std :: endl;
+  std :: cout << p1->getMoves() << std :: endl;
+
+  
   if (c1->win(p1) && p1->win(c1) ){
-    std :: cout << c1->getMove() << ":" << p1-> getMove() << std :: endl;
+    std :: cout << "Computer move: "<< c1->getMove() << "::" << "Person move: "<< p1-> getMove() << std :: endl;
     std :: cout << "Draw" << std :: endl;
 
   }else if (c1->win(p1) == true)
   {
-    std :: cout << c1->getMove() << ":" << p1-> getMove() << std :: endl;
+    std :: cout << "Computer move: "<< c1->getMove() << "::" << "Person move: "<< p1-> getMove() << std :: endl;
     std :: cout << "Computer Win!" << std :: endl;
   }else if (p1->win(c1) == true)
   {
-    std :: cout << c1->getMove() << ":" << p1-> getMove() << std :: endl;  
+    std :: cout << "Computer move: "<< c1->getMove() << "::" << "Person move: "<< p1-> getMove() << std :: endl;
     std :: cout << "Person Win!" << std :: endl;
   }
-  */
+  
+  c1->move();
+  p1->move();
+  std :: cout << c1->getMoves() << std :: endl;
+  std :: cout << p1->getMoves() << std :: endl;
 
 }
